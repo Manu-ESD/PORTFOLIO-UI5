1419f2cdbb412a1a8fecc3631e5266c9c0986863
Manu-ESD <124464914+Manu-ESD@users.noreply.github.com>
2023-07-19 13:25:31 +0530

    Update App.controller.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -23,7 +23,7 @@ function(Controller, MessageToast,  coreLibrary, Dialog, Button, mobileLibrary, 
 				MessageToast.show("Downloading Guggilla Venkata Manohar Resume...");
 
 				var link = document.createElement('a');
-				link.href = "https://drive.google.com/file/d/1U780WdqgOnEMWsQYNYVg4nR__4xWqBO1/view";
+				link.href = "https://drive.google.com/file/d/1iwVgZXODX-SKl44ZaLEQfpmCdXMcJAZG/view";
 				link.download = 'file.txt';
 				document.body.appendChild(link);
 				link.click();
